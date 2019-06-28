19ca5ac95bab4c11a07bad03e1313af01fb916a5
Andrija Perovic <andrija989@gmail.com>
2019-06-28 19:37:10 +0200

    Zadatak 2

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,13 +1,33 @@
 <template>
-    <div></div>
+<div>
+  <ul>
+    <li v-for='(contact,index) in contacts' :key="index">
+      {{ contact.fullName}}
+      <button @click="handleRemove(contact)">Remove</button>
+    </li>
+  </ul>
+</div>
 </template>
 
-<script>
-export default {
 
+<script>
+
+import {customerService} from '@/services/CustomerService'
+export default {
+    data () {
+    return {
+      contacts: customerService.list()
+    }
+  },
+  
+  methods: {
+      handleRemove(contact) {
+          this.$emit(customerService.remove())
+      }
+  }
 }
 </script>
-
+    
 <style>
 
 </style>
